e713521b8ab7bc11f3db012f234794b0af3d569b
Bharosa <bharosa115r@gmail.com>
2025-01-05 14:37:20 +0545

    Added commands.js file

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,16 +23,31 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add("login_user", (username, password) => {
-  cy.visit("https://www.saucedemo.com/");
-  cy.url().should("eq", "https://www.saucedemo.com/");
-  cy.get('[data-test="login-button"]').should("have.value", "Login"); //assertion
-  cy.get('[data-test="login-button"]').should(
-    "have.css",
-    "background-color",
-    "rgb(61, 220, 145)"
-  );
-  cy.get("#user-name").type(username);
-  cy.get("#password").type(password, { log: false });
+
+// Cypress.Commands.add("login_user", (username, password) => {
+//   cy.visit("https://www.saucedemo.com/");
+//   cy.url().should("eq", "https://www.saucedemo.com/");
+//   cy.get('[data-test="login-button"]').should("have.value", "Login"); //assertion
+//   cy.get('[data-test="login-button"]').should(
+//     "have.css",
+//     "background-color",
+//     "rgb(61, 220, 145)"
+//   );
+//   cy.get("#user-name").type(username);
+//   cy.get("#password").type(password, { log: false });
+//   cy.get("#login-button").click();
+// });
+
+Cypress.Commands.add("login_user", (userName, password) => {
+  cy.get("#user-name").type(userName);
+  cy.get("#password").type(password);
   cy.get("#login-button").click();
+});
+
+Cypress.Commands.add("addToCart", (itemId) => {
+  cy.get(`#add-to-cart-${itemId}`).click();
+});
+Cypress.Commands.add("removeFromCart", (itemId) => {
+  cy.log(`Removing item: ${itemId}`);
+  cy.get(`#remove-${itemId}`).click();
 });
